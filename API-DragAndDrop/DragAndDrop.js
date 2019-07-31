bdf4f9eda4aa58d993a9bc990e7dcadf1eb29e38
Alejandro Santoscyo Rivero <45904717+Santocoyo@users.noreply.github.com>
2019-07-31 17:42:39 -0500

    Update DragAndDrop.js

--- API-DragAndDrop/DragAndDrop.js
+++ API-DragAndDrop/DragAndDrop.js
@@ -4,13 +4,25 @@ function comenzar(){
     elem_origen=document.getElementById("imagen");
     elem_origen.addEventListener("dragstart", comenzamos_arrastrar, false);
     elem_destino=document.getElementById("zonadestino");
-    elem_destino.addEventListener("dragenter", function(e){
-        e.preventDefault();
-    }, false);
     elem_destino.addEventListener("dragover", function(e){
         e.preventDefault();
     }, false);
     elem_destino.addEventListener("drop", soltado, false);
+    elem_origen.addEventListener("dragend", terminado, false);
+    elem_destino.addEventListener("dragenter", entrando, false);
+    elem_destino.addEventListener("dragleave", salir, false);
+}
+function salir(e){
+    e.preventDefault();
+    elem_destino.style.background="#FFF";
+}
+function entrando(e){
+    e.preventDefault();
+    elem_destino.style.background="rgba(8,252,25,.8)";
+}
+function terminado(e){
+    var elemento=e.target;
+    elemento.style.visibility="hidden";
 }
 function comenzamos_arrastrar(e){
     var codigo="<img src='"+elem_origen.getAttribute("src")+"'>";
