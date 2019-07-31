10da2388797358f85fe0e91f7ef00fcb6ac9706c
Alejandro Santoscyo Rivero <45904717+Santocoyo@users.noreply.github.com>
2019-07-31 18:44:54 -0500

    Update galeria_arrastre.js

--- API-DragAndDrop/galeria_arrastre.js
+++ API-DragAndDrop/galeria_arrastre.js
@@ -3,6 +3,9 @@ function comenzar(){
     var imagenes=document.querySelectorAll("#cajaimagen img");
     for(var i=0; i<imagenes.length; i++){
         imagenes[i].addEventListener("dragstart", comenzando_arrastrar,false);
+        if(i!=1){
+            imagenes[i].addEventListener("dragend", terminado,false);
+        }
     }
     elem_destino=document.getElementById("zonadestino");
     elem_destino.addEventListener("dragenter", entrando,false);
@@ -12,7 +15,13 @@ function comenzar(){
 }
 function entrando(e){
     e.preventDefault();
-    elem_destino.style.background="rgba(8,252,25,0.8)";
+    var id=e.dataTransfer.getData("Text");
+    if(id!="minecraft"){
+        elem_destino.style.background="rgba(8,252,25,0.8)";
+    }
+    else{
+        elem_destino.style.background="#fa0d29";
+    }
 }
 function comenzando_arrastrar(e){
     var elemento=e.target;
@@ -22,11 +31,20 @@ function comenzando_arrastrar(e){
 function soltado(e){
     e.preventDefault();
     var id=e.dataTransfer.getData("Text");
-    var src=document.getElementById(id).src;
-    elem_destino.innerHTML="<img src='"+src+"'>";
+    if(id!="minecraft"){
+        var src=document.getElementById(id).src;
+        elem_destino.innerHTML="<img src='"+src+"'>";
+    }else{
+        elem_destino.innerHTML="La imagen no es admitida"
+        elem_destino.style.background="#fa0d29";
+    }
 }
 function saliendo(e){
     e.preventDefault();
     elem_destino.style.background="#FFF";
+}
+function terminado(e){
+    var elemento=e.target;
+    elemento.style.visibility="hidden";
 }
 window.addEventListener("load", comenzar, false);
