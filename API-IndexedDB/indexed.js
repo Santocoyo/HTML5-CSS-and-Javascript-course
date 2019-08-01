298ca622e30b7b88884ef1180f2d8dde21dea0a2
Alejandro Santoscyo Rivero <45904717+Santocoyo@users.noreply.github.com>
2019-08-01 15:58:36 -0500

    Update indexed.js

--- API-IndexedDB/indexed.js
+++ API-IndexedDB/indexed.js
@@ -15,8 +15,23 @@ function agregarobjeto(){
     var transaccion = bd.transaction(["gente"], "readwrite");
     var almacen = transaccion.objectStore("gente");
     var agregar = almacen.add({clave: clave, titulo: titulo, fecha: fecha});
+    agregar.addEventListener("success", mostrar, false);
     document.getElementById("clave").value="";
     document.getElementById("texto").value="";
     document.getElementById("fecha").value="";
+}
+function mostrar(){
+    zonadatos.innerHTML="";
+    var transaccion = bd.transaction(["gente"],"readonly");
+    var almacen = transaccion.objectStore("gente");
+    var cursor = almacen.openCursor();
+    cursor.addEventListener("success", mostrarDatos, false);
+}
+function mostrarDatos(e){
+    var cursor=e.target.result;
+    if(cursor){
+        zonadatos.innerHTML+="<div>" + cursor.value.clave + " - " + cursor.value.titulo + " - " + cursor.value.fecha + "</div>";
+        cursor.continue();
+    }
 }
 window.addEventListener("load", comenzar, false);
