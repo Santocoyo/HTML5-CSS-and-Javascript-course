bf3d5156e97175ebc5f61204e776f0990bcf804f
Alejandro Santoscyo Rivero <45904717+Santocoyo@users.noreply.github.com>
2019-08-02 17:02:37 -0500

    Add script

---
+++ API-File/archivos.js
@@ -0,0 +1,17 @@
+function iniciar(){
+    zonadatos=document.getElementById("zonadatos");
+    var archivos = document.getElementById("archivos");
+    archivos.addEventListener("change", procesar, false);
+}
+function procesar(e){
+    var archivos = e.target.files;
+    var mi_archivo=archivos[0];
+    var lector = new FileReader();
+    lector.readAsText(mi_archivo, "iso-8859-1");
+    lector.addEventListener("load", mostrar_en_web, false);
+}
+function mostrar_en_web(e){
+    var resultado=e.target.result;
+    zonadatos.innerHTML=resultado;
+}
+window.addEventListener("load", iniciar, false);
