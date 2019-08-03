d59e571cb2a7c483a2d88682638a6ba558339595
Alejandro Santoscyo Rivero <45904717+Santocoyo@users.noreply.github.com>
2019-08-03 16:53:10 -0500

    Update apifile.js

--- API-File/apifile.js
+++ API-File/apifile.js
@@ -9,18 +9,40 @@ function acceso(){
 }
 function crearsis(sistema){
     espacio=sistema.root;
+    ruta="";
+    mostrar();
 }
 function crear(){
     var nombre_archivo=document.getElementById("entrada").value;
     if(nombre_archivo!=""){
-        espacio.getFile(nombre_archivo, {get: true, exclusive: false}, mostrar, errores);
+        nombre_archivo=ruta+nombre_archivo;
+        espacio.getFile(nombre_archivo, {create:true, exclusive:false}, mostrar, errores);
     }
 }
-function mostrar(entrada){
+function mostrar(){
     document.getElementById("entrada").value="";
-    zonadatos.innerHTML="Éxito en la creación de espacio y archivo <br>";
-    zonadatos.innerHTML+="Nombre: " + entrada.name + "<br";
-    zonadatos.innerHTML+="Ruta: " + entrada.fullPath + "<br>";
+    zonadatos.innerHTML="";
+    espacio.getDirectory(ruta, null, leerdir, errores);
+}
+function leerdir(dir){
+    lector=dir.createReader();
+    leer();
+}
+function leer(){
+    lector.readEntries(function(archivos){
+        if(archivos.length){
+            listar(archivos);
+        }
+    }, errores);
+}
+function listar(archivos){
+    for(var i=0; i<archivos.length;i++){
+        if(archivos[i].isFile){
+            zonadatos.innerHTML+=archivos[i].name+"<br>";
+        }else if(archivos[i].isDirectory){
+            zonadatos.innerHTML+="<span class='directorio'>" + archvios[i].name + "</span><br>";
+        }
+    }
 }
 function errores(e){
     alert("Ha habido un error: " + e.code);
