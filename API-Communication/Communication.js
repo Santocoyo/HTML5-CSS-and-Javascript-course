49b99f758f6ebebcba386a84f78aacc5b98b6a44
Alejandro Santoscyo Rivero <45904717+Santocoyo@users.noreply.github.com>
2019-08-07 10:49:58 -0500

    Update Communication.js

--- API-Communication/Communication.js
+++ API-Communication/Communication.js
@@ -1,16 +1,28 @@
 function comenzar(){
     zonadatos = document.getElementById("zonadatos");
+    zonaprogreso = document.getElementById("zonaprogreso");
     var boton = document.getElementById("boton");
     boton.addEventListener("click", leer, false);
 }
 function leer(){
-    var url = "texto.txt";
+    var url = "video.mp4";
     var solicitud = new XMLHttpRequest();
+    solicitud.addEventListener("loadstart", comienza_barra, false);
+    solicitud.addEventListener("progress", estado_barra, false);
     solicitud.addEventListener("load", mostrar, false);
     solicitud.open("GET", url, true);
     solicitud.send(null);
 }
 function mostrar(e){
-    zonadatos.innerHTML = e.target.responseText;
+    zonadatos.innerHTML = "¡Archivo leído!";
+}
+function comienza_barra(){
+    zonadatos.innerHTML="<progress value='0' max='100'></progress>";
+}
+function estado_barra(e){
+    var porcentaje = parseInt(e.loaded/e.total*100);
+    var barraprogreso = zonadatos.querySelector("progress");
+    barraprogreso.value = porcentaje;
+    zonaprogreso.innerHTML = porcentaje + " %";
 }
 window.addEventListener("load", comenzar, false);
