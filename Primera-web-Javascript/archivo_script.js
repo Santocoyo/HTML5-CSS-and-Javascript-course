857a4067fc808b43dd2445e8ba57fa89fdaac8ee
Alejandro Santoscoy Rivero <45904717+Santocoyo@users.noreply.github.com>
2019-08-11 17:51:15 -0500

    Add files via upload

---
+++ Primera-web-Javascript/archivo_script.js
@@ -0,0 +1,12 @@
+function ejecuta(){
+    //document.querySelector("#principal p:last-child").onclick=saludo;
+    var elementos=document.querySelectorAll("#principal p, span");
+    for(var i=0;i<elementos.length;i++){
+        elementos[i].onclick=saludo;
+    }
+    
+}
+function saludo(){
+    alert("Qué hay de nuevo");
+}
+window.onload=ejecuta;
